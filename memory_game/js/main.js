d55845605fa32734c3581c6659152b37c4d3fda2
Danny Wong <wong.danny29@gmail.com>
2018-11-24 14:09:42 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,23 @@
 console.log("Up and Running");
 
-var cardOne = "queen"; 
-var cardTwo = "queen"; 
-var cardThree = "king";
-var cardFour = "king";
+
+
+
+var cards = ["king", "king", "queen", "queen"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
 
 console.log("User Flipped " + cardOne);
 console.log("User Flipped " + cardTwo);
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
